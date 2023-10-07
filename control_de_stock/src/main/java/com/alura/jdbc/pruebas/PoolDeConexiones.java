6fa645e5c4d6c6df1ac1ead4c871d7d43034fb61
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-10-07 02:01:47 -0600

    control_de_stock final project

---
+++ control_de_stock/src/main/java/com/alura/jdbc/pruebas/PoolDeConexiones.java
@@ -0,0 +1,21 @@
+package com.alura.jdbc.pruebas;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.alura.jdbc.factory.ConnectionFactory;
+
+public class PoolDeConexiones {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		
+		for(int i = 0; i< 20; i++)
+		{
+			Connection con = connectionFactory.recuperaConexion();
+			
+			System.out.println("Abriendo la conexion de numero " + (i+1));
+		}
+	}
+}
